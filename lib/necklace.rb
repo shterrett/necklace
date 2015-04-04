96cf4a0bc497cc26c9bc492b117525c964c4b2b0
Stuart Terrett <shterrett@gmail.com>
2015-04-04 15:21:22 -0400

    Refactors through_* methods to share code

--- lib/necklace.rb
+++ lib/necklace.rb
@@ -1,27 +1,25 @@
 module Necklace
+  ARG_INSERTION_ORDER = { first: 1, last: -1 }
+
   def through(enumerable, methods)
-    methods.reduce(enumerable) do |result, method|
-      send(method, result)
-    end
+    through_last(enumerable, methods)
   end
 
   def through_first(enumerable, methods)
     methods.reduce(enumerable) do |result, method|
-      if method.respond_to? :first
-        send(method.first, result, *method.drop(1))
-      else
-        send(method, result)
-      end
+      _call(method, result, :first)
     end
   end
 
   def through_last(enumerable, methods)
     methods.reduce(enumerable) do |result, method|
-      if method.respond_to? :first
-        send(method.first, *method.drop(1), result)
-      else
-        send(method, result)
-      end
+      _call(method, result, :last)
     end
+  end
+
+  def _call(method, result, order)
+    method = [method].flatten
+    args = method.insert(ARG_INSERTION_ORDER[order], result)
+    send(*args)
   end
 end
